7db3a38615159c159aa2cac78bf3830cf0915ac3
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 15:37:03 +0530

    shipping update

--- shipping.sh
+++ shipping.sh
@@ -5,6 +5,7 @@ java_call
 
 echo_line "copy db schema, user to mysql server"
 mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql &>>${log_file}
-mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql &>>${log_file}
 status_check "copy db schema, user to mysql server"
+mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql &>>${log_file}
+status_check "copy app user schema to mysql server"
 
