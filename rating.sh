38bad74d15c1fe6c17c3edbdf76fc0967c58cb43
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 18:49:55 +0530

    rating

--- rating.sh
+++ rating.sh
@@ -2,16 +2,22 @@ component=$1
 source "$(dirname "${component}.sh")/common.sh"
 
 pre_req
-echo "$YELLOW>>>>>>> Install Python 3 <<<<<<<<<$RESET"
+echo "${YELLOW}>>>>>>> Install Python 3 <<<<<<<<<${RESET}"
+pyth_call
 
 dnf install -y mysql8.4
-
-
-mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql
-mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql
+status_check "install mysql"
 
 echo "$YELLOW>>>>>>>Deploy Application <<<<<<<<<$RESET"
 pip3 install -r /app/requirements.txt cryptography
+
+
+mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql
+status_check "db schema"
+mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql
+status_check "app user db schema
+"
+
 
 
 systemd_call
