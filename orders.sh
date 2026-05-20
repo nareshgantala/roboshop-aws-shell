2c02d6ef7d5ca50f5195e88cccb2bd2cbb9d02d4
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 15:38:59 +0530

    orders update

--- orders.sh
+++ orders.sh
@@ -1,6 +1,4 @@
 component=$1
 source "$(dirname "${component}.sh")/common.sh"
 java_call
-echo "$YELLOW>>>>>>> Configure Systemd Service<<<<<<<<<$RESET"
-cp orders.service /etc/systemd/system/orders.service
-systemd_call
+
