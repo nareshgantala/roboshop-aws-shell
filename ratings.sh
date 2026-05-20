59638107d28e59ad44c11935145d37bf78ea112b
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 15:43:16 +0530

    ratings

--- ratings.sh
+++ ratings.sh
@@ -4,14 +4,13 @@ source "$(dirname "${component}.sh")/common.sh"
 pre_req
 echo "$YELLOW>>>>>>> Install Python 3 <<<<<<<<<$RESET"
 
-dnf install -y python3 python3-pip mysql8.4
+dnf install -y mysql8.4
 
 
 mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql
 mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql
 
 echo "$YELLOW>>>>>>>Deploy Application <<<<<<<<<$RESET"
-
 pip3 install -r /app/requirements.txt cryptography
 
 
