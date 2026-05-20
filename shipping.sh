cdba3d1437d08c40b615a5dd714053ae7ebad8b4
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 15:33:59 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ source "$(dirname "${component}.sh")/common.sh"
 java_call
 
 echo_line "copy db schema, user to mysql server"
-mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql
-mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql
+mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/schema.sql &>>${log_file}
+mysql -h mysql.naresh-training.online -u root -pRoboShop@1 < db/app-user.sql &>>${log_file}
 status_check "copy db schema, user to mysql server"
 
--- user.sh
+++ user.sh
@@ -1,6 +1,5 @@
 component=$1
 source "$(dirname "${component}.sh")/common.sh"
-
 nodejs_call
 
 
