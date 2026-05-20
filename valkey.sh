e58f3a615604c4d2999a8dff39c04b27d53d2fc1
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 15:10:56 +0530

    variable updated

--- valkey.sh
+++ valkey.sh
@@ -1,13 +1,13 @@
 component=$1
 source "$(dirname "${component}.sh")/common.sh"
 
-echo "$YELLOW>>>>>>> Install Valkey <<<<<<<<<$RESET" | tee -a ${log_file}
+echo "${YELLOW}>>>>>>> Install Valkey <<<<<<<<<${RESET}" | tee -a ${log_file}
 
 dnf install -y valkey &>>${log_file}
 systemctl enable valkey &>>${log_file}
 systemctl start valkey &>>${log_file}
 
-echo "$YELLOW>>>>>>> configure valkey <<<<<<<<<$RESET" | tee -a ${log_file}
+echo "${YELLOW}>>>>>>> configure valkey <<<<<<<<<${RESET}" | tee -a ${log_file}
 
 sed -i "s/bind 127.0.0.1/bind 0.0.0.0/" /etc/valkey/valkey.conf &>>${log_file}
 sed -i "s/protected-mode yes/protected-mode no" /etc/valkey/valkey.conf &>>${log_file}
