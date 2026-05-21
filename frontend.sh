77729d339cb6e08bccec3604d19164db70e34b44
Naresh Gantala <nareshgantala@gmail.com>
2026-05-21 13:48:07 +0530

    added unzip in seperate

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,11 @@
 component=frontend
 
 source "$(dirname "${component}.sh")/common.sh"
+dnf install -y unzip &>>${log_file}
+status_check "Install unzip"
 
 echo_line "Install Nginx 1.26"
-dnf install -y nginx unzip &>>${log_file}
+dnf install -y nginx &>>${log_file}
 status_check "Install Nginx 1.26"
 
 echo_line "Configure Nginx" &>>${log_file}
