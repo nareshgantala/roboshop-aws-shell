74bfb11efa6f2ba136f14483cad751329a0c7b7e
Naresh Gantala <nareshgantala@gmail.com>
2026-05-21 18:14:35 +0530

    shpping code update

--- shipping.sh
+++ shipping.sh
@@ -1,8 +1,8 @@
-component=$1
+component=shipping
 source "$(dirname "${component}.sh")/common.sh"
 
 schema=mysql
-schema_type="db/schema.sql db/app-user.sql"
+schema_type="schema.sql app-user.sql"
 
 schema_load
 java_call
